fd29df3d3604b792262e3685bf4a5657003a987e
Aleksandr Kretov <kretov_a@outlook.com>
2022-05-06 21:09:05 +0300

    1st commit

---
+++ src/test/java/ru/netology/Tests.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class Tests {
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+
+
+}
